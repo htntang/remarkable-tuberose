80f819a6f5386b25af609c1ecf34e0dc8c89b634
htntang <htntang@gmail.com>
2023-09-04 06:53:01 -0600

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navigation from "./components/Navbar";
+
 const App = () => {
-
   return (
-    <div>
-      Girly Pop 3D Developer Portfolio
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-beige">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navigation />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
